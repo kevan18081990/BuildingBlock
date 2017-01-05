37c7a2da7578a766ab837449798c9dca6b55dcc4
kevan18081990 <kevan18081990@gmail.com>
2017-01-05 18:05:48 +0530

    Example Activity is created with Fragment dialog

--- app/src/main/java/com/example/kevan/buildingblock/ExampleActivity.java
+++ app/src/main/java/com/example/kevan/buildingblock/ExampleActivity.java
@@ -1,9 +1,11 @@
 package com.example.kevan.buildingblock;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 //TODO: put activity lifecycle on the layout of this activity
 
@@ -14,71 +16,78 @@ public class ExampleActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example);
-        //findViews();
-        notify("onCreate");
+        findViews();
+        notify("onCreate: Activity is created");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        notify("onStart: Activity is visible");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        notify("onPause");
+        notify("onPause: Activity is not interactable");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        notify("onResume");
+        notify("onResume: Activity is interactable");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        notify("onStop");
+        notify("onStop: Activity is not visible");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        notify("onDestroy");
+        notify("onDestroy: Activity is destroyed");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        notify("onRestart");
+        notify("onRestart: Activity is restarted after stopped and before destroyed");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        notify("onRestoreInstanceState");
+        notify("onRestoreInstanceState: called before onResume if onSaveInstance had been called first");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        notify("onSaveInstanceState");
+        notify("onSaveInstanceState: called after onPause");
     }
 
     private void notify(String methodName) {
-        Toast.makeText(this,methodName,Toast.LENGTH_SHORT).show();
+        Log.i("ExampleActivity",methodName);
+        //Toast.makeText(this,methodName,Toast.LENGTH_SHORT).show();
     }
 
-//    private void findViews(){
-//        btnOpenDialog = (Button) findViewById(R.id.btnOpenDialog);
-//        btnOpenDialog.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                showNoticeDialog();
-//            }
-//        });
-//    }
-//
-//    public void showNoticeDialog() {
-//        // Create an instance of the dialog fragment and show it
-//        DialogFragment dialog = new NoticeDialogFragment();
-//        dialog.show(getSupportFragmentManager(), "NoticeDialogFragment");
-//    }
+    private void findViews(){
+        btnOpenDialog = (Button) findViewById(R.id.btnOpenDialog);
+        btnOpenDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNoticeDialog();
+            }
+        });
+    }
+
+    public void showNoticeDialog() {
+        // Create an instance of the dialog fragment and show it
+        DialogFragment dialog = new ExampleDialogFragment();
+        dialog.show(getSupportFragmentManager(), "Example Dialog Fragment");
+    }
 
 
 
---
+++ app/src/main/java/com/example/kevan/buildingblock/ExampleDialogFragment.java
@@ -0,0 +1,32 @@
+package com.example.kevan.buildingblock;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by Kevan on 1/5/2017.
+ */
+
+public class ExampleDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Dialog")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
